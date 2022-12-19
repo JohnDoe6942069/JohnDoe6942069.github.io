ed3f05958179f2c051f6f9136a766589015d1da5
JohnDoe6942069 <120896116+JohnDoe6942069@users.noreply.github.com>
2022-12-19 00:20:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,46 @@ function displaytime(){
     var hour = d.getHours();
     var min = d.getMinutes();
     var sec = d.getSeconds();
-    document.getElementById("clock").innerHTML = hour+ ":" +min +":"+sec;
+    if(hour>12){
+        var meridian = "PM";
+
+    }
+    else{
+        var meridian = "AM";
+    }
+    if(min>10){
+        min="0"+min
+
+    }
+
+    if (hour>12){
+        hour=hour-12;
+    }
+    if (hour==0){
+        hour=hour+12;
+    }
+    document.getElementById("clock").innerHTML = hour+ ":" +min +" "+meridian;
 }
 setInterval(displaytime, 1000)
+
+
+
+
+
+function dinnerTime(){
+    var time = new Date();
+    var hour2 = time.getHours();
+    var min2 = time.getMinutes();
+    if (hour2>12){
+        hour2=hour2-12;
+    }
+
+    if(hour2==8 && min2== 0){
+        var variable = "YES";
+    }
+ else{
+        var variable = "No!";
+    }
+    document.getElementById("alarm").innerHTML = variable;}
+setInterval(dinnerTime,1000)
+
